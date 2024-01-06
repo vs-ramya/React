bc7e28c1d6cd601f83eed65404a435bae38156b2
vs-ramya <sramya8051@gmail.com>
2024-01-06 22:46:54 +0530

    States in react example

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,26 @@
-import ExpenseDate from './ExpenseDate';
+ import React,{useState} from 'react';
+ import ExpenseDate from './ExpenseDate';
+
 import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  function clickHandler() {
-    const parentElement =  document.querySelector('.expense-item');
-    parentElement.remove();
-  }
+  //setAmount is a function used to update the value of amount   and usestate used to inialize the amount
+  //const [amount, setAmount] uses array destructuring to assign names to the elements returned by the useState hook.
+  //The first element (amount) represents the current state value, and the second element (setAmount) is the function that can be used to update the state variable amount
+    const [amount,setAmount]=useState(props.amount);
+    function clickHandler() {
+      setAmount('$100');
+      console.log(amount);
+    }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-        <button onClick={clickHandler}>Delete</button>
+        <div className='expense-item__price'>{amount}</div>
+        <button onClick={clickHandler}>Change Amount</button>
       </div>
     </Card>
   );
