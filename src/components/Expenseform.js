8cd3b8a8be62c6b4574c99fecda22070cd033f32
vs-ramya <sramya8051@gmail.com>
2024-01-07 15:27:21 +0530

    Creates object on submitting

--- src/components/Expenseform.js
+++ src/components/Expenseform.js
@@ -1,32 +1,36 @@
-import React,{useState} from "react";
+//import React,{useState} from "react";
 function Expenseform() {  
-        const [enteredTitle, setEnteredTitle] = useState('');
-        const [enteredAmount, setEnteredAmount] = useState('');
-        const [enteredDate, setEnteredDate] = useState('');
     
-        function titleChangeHandler(event) {
-            setEnteredTitle(event.target.value);
-        }
-    
-        function amountChangeHandler(event) {
-            setEnteredAmount(event.target.value);
-        }
-    
-        function dateChangeHandler(event) {
-            setEnteredDate(event.target.value);
+    function clickHandler(event) {
+            event.preventDefault();
+            let newExpense=document.getElementById('name');
+    let newAmount=document.getElementById('money');
+    let newDate=document.getElementById('calendar');
+    let name=newExpense.value;
+    let amount=newAmount.value;
+    let Date= newDate.value;
+            let obj={
+                ExpenseName:name,
+                ExpenseAmount:amount,
+                ExpenseDate:Date,
+
+            }
+            console.log(obj);
+
+
         }
     return (
         <div>
             <form>
                 <label>Expense Title: </label>
                 {/* The onchange event is an event handler in HTML& JS that is triggered when the value of an input  is changed by the user */}
-                <input type='text' name="title" id='name' value={enteredTitle} onChange={titleChangeHandler}></input>
+                <input type='text' name="title" id='name'></input>
                 <label>Expense Amount: </label>
                 {/*This value={enteredAmount} may be declared or even nothing happens whwn you didnt even declare it */}
-                <input type='text' name="amount" value={enteredAmount} onChange={amountChangeHandler}></input>
+                <input type='text' name="amount" id='money'  ></input>
                 <label>Date: </label>
-                <input type='date' min='2022-01-01' max='2023-12-31' name="date" value={enteredDate} onChange={dateChangeHandler}></input> 
-                <span> <button type="submit">Add Expense</button></span>
+                <input type='date' min='2022-01-01' max='2023-12-31' name="date" id='calendar'></input> 
+                <span> <button type="submit" onClick={clickHandler}>Add Expense</button></span>
                 
         
             </form>
@@ -36,3 +40,5 @@ function Expenseform() {
     )
 }
 export default Expenseform;
+  
+        
