13e9cbde70bc293605c7af95accab42b1802ce59
vs-ramya <sramya8051@gmail.com>
2024-01-07 12:15:36 +0530

    Adding multiple states to a single component

--- src/components/Expenseform.js
+++ src/components/Expenseform.js
@@ -1,18 +1,31 @@
-function Expenseform() {
-    function titleChangeHandler(event) {
-        
-        console.log(event.target.value);
-    }
+import React,{useState} from "react";
+function Expenseform() {  
+        const [enteredTitle, setEnteredTitle] = useState('');
+        const [enteredAmount, setEnteredAmount] = useState('');
+        const [enteredDate, setEnteredDate] = useState('');
+    
+        function titleChangeHandler(event) {
+            setEnteredTitle(event.target.value);
+        }
+    
+        function amountChangeHandler(event) {
+            setEnteredAmount(event.target.value);
+        }
+    
+        function dateChangeHandler(event) {
+            setEnteredDate(event.target.value);
+        }
     return (
         <div>
             <form>
                 <label>Expense Title: </label>
                 {/* The onchange event is an event handler in HTML& JS that is triggered when the value of an input  is changed by the user */}
-                <input type='text' name="title" id='name' onChange={titleChangeHandler}></input>
+                <input type='text' name="title" id='name' value={enteredTitle} onChange={titleChangeHandler}></input>
                 <label>Expense Amount: </label>
-                <input type='text' name="amount" onChange={titleChangeHandler}></input>
+                {/*This value={enteredAmount} may be declared or even nothing happens whwn you didnt even declare it */}
+                <input type='text' name="amount" value={enteredAmount} onChange={amountChangeHandler}></input>
                 <label>Date: </label>
-                <input type='date' min='2022-01-01' max='2023-12-31' name="date" onChange={titleChangeHandler}></input> 
+                <input type='date' min='2022-01-01' max='2023-12-31' name="date" value={enteredDate} onChange={dateChangeHandler}></input> 
                 <span> <button type="submit">Add Expense</button></span>
                 
         
