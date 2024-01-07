c3d2ccb0491f38123eeb0ddbb67ce73721ed173c
vs-ramya <sramya8051@gmail.com>
2024-01-07 11:40:56 +0530

    onChange event

--- src/components/Expenseform.js
+++ src/components/Expenseform.js
@@ -1,14 +1,19 @@
 function Expenseform() {
+    function titleChangeHandler(event) {
+        
+        console.log(event.target.value);
+    }
     return (
         <div>
             <form>
-                <label for='name'>Expense Title: </label>
-                <input type='text' name="title" id='name'></input>
-                <label for='money'>Expense Amount: </label>
-                <input type='text' name="amount"></input>
-                <label for='calendar'>Date: </label>
-                <input type='date' name="date"></input> 
-                <span> <button>Add Expense</button></span>
+                <label>Expense Title: </label>
+                {/* The onchange event is an event handler in HTML& JS that is triggered when the value of an input  is changed by the user */}
+                <input type='text' name="title" id='name' onChange={titleChangeHandler}></input>
+                <label>Expense Amount: </label>
+                <input type='text' name="amount" onChange={titleChangeHandler}></input>
+                <label>Date: </label>
+                <input type='date' min='2022-01-01' max='2023-12-31' name="date" onChange={titleChangeHandler}></input> 
+                <span> <button type="submit">Add Expense</button></span>
                 
         
             </form>
